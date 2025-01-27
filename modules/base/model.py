600bb630438c0c69c5532a324fc0c74780c16c7b
Faris <madebyais@gmail.com>
2025-01-27 14:40:17 +0700

    refactor: add new service result model to modules/base

--- modules/base/model.py
+++ modules/base/model.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel, Field
 
 class RepositoryResultModel(BaseModel):
     data: Optional[Any] = Field(default=None)
+
+
+class ServiceResultModel(BaseModel):
+    success: bool = Field(default=False)
+    data: Optional[Any] = Field(default=None)
+    message: Optional[str] = Field(default="Success")
+    error_msg: Optional[str] = Field(default="Something went wrong. Please try again.")
